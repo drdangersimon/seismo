b02e7838191a04615210ca3cc104866fc2e04aad
Thuso Simon <dr.danger.simon@gmail.com>
2014-11-28 10:34:11 +0200

    Revert "Merge pull request #7 from ezietsman/develop"
    This reverts commit 43729526391126c025bf13d727edb693cd46daa9, reversing
    changes made to 44db8e4df1d657a6f3b629efa6c396ac0bc56ec2.

--- seismo/__init__.py
+++ seismo/__init__.py
@@ -1,4 +1,4 @@
-from .timeseries import deeming, fast_deeming, find_peak
+from .timeseries import deeming, fast_deeming
 from .fitting import signal, sinewave
 from .session import Session
 from .lomb_scargle import lomb_scargle, lomb_scargle32
--- seismo/fitting.py
+++ seismo/fitting.py
@@ -9,7 +9,12 @@ class signal(object):
     '''
 
     def __init__(self):
-        ''' Initialise signal. '''
+        ''' Initialise signal.
+        Inputs:
+            x: numpy array with e.g. timestamps
+            y: numpy array with value at timestamps
+            err: numpy array with error estimates
+        '''
         self.components = []
 
     def add_component(self, component):
